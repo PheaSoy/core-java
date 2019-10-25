a2097661d685d7be1998fe5047c67a5f4797bc22
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2019-10-25 09:38:27 +0700

    Added comment for easy to understand.

--- src/main/java/java8/function/Function1.java
+++ src/main/java/java8/function/Function1.java
@@ -9,26 +9,23 @@ public class Function1 {
     public static Logger logger = Logger.getLogger(Function1.class);
 
     public static void main(String args[]) {
+
         logger.info("Increased + Doubled:"+increased_double(10));
 
-        Function<Integer, Integer> multiply = (value) -> value * 2;
+
         Function<Integer, Integer> add      = (value) -> value + 3;
+        Function<Integer, Integer> multiply = (value) -> value * 2;
 
-        Function<Integer, Integer> addThenMultiply = multiply.compose(add);
-
-        Integer result1 = addThenMultiply.apply(3);
-        System.out.println(result1);
-
-
-        Function<Integer, Integer> multiplyThenAdd = multiply.andThen(add);
-
+        Function<Integer, Integer> multiplyThenAdd = multiply.andThen(add); //9 execute the left side first (2*3) +3
         Integer result2 = multiplyThenAdd.apply(3);
-        System.out.println(result2);
+        System.out.println("andThen:"+result2);
 
-        Function<Integer, Integer>  multiplyThenAdd1 = add.compose(multiply);
+        Function<Integer, Integer> addThenMultiply = multiply.compose(add);//12 execute the right side first. (3+3) *2
+        Integer result1 = addThenMultiply.apply(3);
+        System.out.println("Compose:"+result1);
 
-        Integer result3 = multiplyThenAdd1.apply(3);
-        System.out.println(result3);
+
+
 
 
     }
